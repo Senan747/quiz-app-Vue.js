a6d48c9a497af6bd3c3a4fe1cdc3c4cd2f2df2fd
Senan747 <111648854+Senan747@users.noreply.github.com>
2023-03-11 07:46:36 +0400

    second

--- src/App.vue
+++ src/App.vue
@@ -5,28 +5,29 @@
       <input v-model.trim="search" type="text" placeholder="Search...">
     </header>
     <div class="options-container">
-      <div v-for="quiz in quizes" :key="quiz.id" class="card">
-        <img :src="quiz.img" alt="">
+      <!-- <div v-for="quiz in quizes" :key="quiz.id" class="card"> -->
+        <!-- <img :src="quiz.img" alt="">
         <div class="card-text">
           <h2>{{ quiz.name }}</h2>
           <p>{{ quiz.questions.length }} questions</p>
         </div>
-      </div>
-      
+      </div> -->
+      <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz" />
     </div>
   </div>
 </template>
 <script setup>
-import q from "./data/quizes.json"
-import {ref} from "vue"
+  import q from "./data/quizes.json"
+  import {ref, watch} from "vue"
+  import Card from "./components/Card.vue"
 
-const quizes = ref(q)
-const search = ref("")
+  const quizes = ref(q)
+  const search = ref("")
 
-watch(search, () => {
-  console.log("Hello from watch")
-})
-</script>
+  watch(search, () => {
+    quizes.value = q.filter(quiz => quiz.name.toLowerCase().includes(search.value.toLowerCase()))
+  })
+</script> 
 
 <style scoped>
   .container {
@@ -59,27 +60,5 @@ watch(search, () => {
     flex-wrap: wrap;
     margin-top: 40px;
   }
-  .card {
-    width: 310px;
-    overflow: hidden;
-    border-radius: 2%;
-    box-shadow: 1px 1px 10px rgb(48, 48, 48);
-    margin-bottom: 35px;
-    margin-right: 20px;
-    cursor: pointer;
-  }
-
-  .card img {
-    width: 100%;
-    height: 190px;
-    margin: 0;
-  }
-
-  .card .card-text {
-    padding: 0 5px;
-  }
-
-  .card .card-text h2{
-    font-weight: bold;
-  }
+  
 </style>
