66805314f38bb5797cd854b0bad9716c8cb6f800
Senan747 <111648854+Senan747@users.noreply.github.com>
2023-03-17 05:42:20 +0400

    last ones

---
+++ src/components/QuizHeader.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+            <h4>Question 1/3</h4>
+            <div class="bar">
+                <div class="completion"></div>
+            </div>
+    </header>
+</template>
+
+<style scoped>
+  h4{
+    font-size: 40px;
+  }
+  .bar {
+    border: 2px solid bisque;
+    width: 300px;
+    height: 40px;
+
+  }
+  .completion {
+    width: 33%;
+    background-color: bisque;
+  }
+</style>
